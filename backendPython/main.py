a1869cac6c94efc43e226402fbea0f01d3877f79
Mateusz <matimusial5@gmail.com>
2024-10-05 13:52:27 +0200

    delete predict

--- backendPython/main.py
+++ backendPython/main.py
@@ -1,13 +1,52 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from services.bert_predict import load_model_and_tokenizer, predict_top_k
 
 app = FastAPI()
 
+model = None
+tokenizer = None
+label_encoder = None
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+class CategoryRequest(BaseModel):
+    title: str
+    k: int
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.on_event("startup")
+async def startup_event():
+    global model, tokenizer, label_encoder
+    model, tokenizer, label_encoder = load_model_and_tokenizer(accuracy="0.9228")
+
+
+@app.post("/getcategory")
+async def get_category(data: CategoryRequest):
+    """
+    :param:
+    {
+      "title": "",
+      "k":
+    }
+    :return:
+    {
+      "category_1": {
+        "category": "",
+        "score":
+      }
+    }
+    """
+    title = data.title
+    k = data.k
+
+    predicted_categories = predict_top_k(title, model, tokenizer, label_encoder, k=k)
+
+    response = {}
+    for i, (category, score) in enumerate(predicted_categories, 1):
+        response[f"category_{i}"] = {
+            "category": category,
+            "score": float(score)
+        }
+
+    return response
+
---
+++ backendPython/services/bert_predict.py
@@ -0,0 +1,37 @@
+import os
+import pickle
+import tensorflow as tf
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+
+
+def load_model_and_tokenizer(accuracy):
+    from bertTrainer.bert_config import BERT_MODEL_PATH
+
+    model = TFAutoModelForSequenceClassification.from_pretrained(os.path.join(BERT_MODEL_PATH, accuracy, "model"))
+
+    tokenizer = AutoTokenizer.from_pretrained(os.path.join(BERT_MODEL_PATH, accuracy, "tokenizer"))
+
+    with open(os.path.join(BERT_MODEL_PATH, accuracy, "encoder"), 'rb') as f:
+        label_encoder = pickle.load(f)
+
+    return model, tokenizer, label_encoder
+
+
+def predict_top_k(text, model, tokenizer, label_encoder, k):
+    inputs = tokenizer(
+        text,
+        padding=True,
+        truncation=True,
+        max_length=128,
+        return_tensors="tf"
+    )
+
+    logits = model(**inputs).logits
+    probabilities = tf.nn.softmax(logits, axis=-1).numpy()[0]
+
+    top_k_values, top_k_indices = tf.nn.top_k(logits, k=k)
+    top_k_indices = top_k_indices.numpy()[0]
+    normalized_scores = probabilities[top_k_indices]
+    predicted_labels = label_encoder.inverse_transform(top_k_indices)
+
+    return list(zip(predicted_labels, [round(score, 3) for score in normalized_scores]))
